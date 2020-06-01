beb619541bf6c0351a8768104dd9ef6eac45b439
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-01 16:35:09 +0530

    added the click button event on Roll

--- app.js
+++ app.js
@@ -8,3 +8,60 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0]
+roundScore = 0;
+activePlayer = 1;
+
+
+console.log(dice);
+
+// document.querySelector('#current-' + activePlayer).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click',()=>{
+
+    dice = Math.floor(Math.random() * 6) + 1;
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
