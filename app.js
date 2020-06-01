f508af50600ee623979cfb69cd25c5650c9cafc2
namanmanchanda09 <naman.mohan09@outlook.com>
2020-06-02 00:03:19 +0530

    added initialisation function

--- app.js
+++ app.js
@@ -9,26 +9,11 @@ GAME RULES:
 
 */
 
+
 var scores, roundScore, activePlayer, dice;
 
-scores = [0,0]
-roundScore = 0;
-activePlayer = 0;
-
-
-console.log(dice);
-
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-
-var x = document.querySelector("#score-0").textContent;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
+ 
 
 
 document.querySelector('.btn-roll').addEventListener('click',()=>{
@@ -47,7 +32,7 @@ document.querySelector('.btn-roll').addEventListener('click',()=>{
 });
 
 
-document.querySelector('.btn-hold').addEventListener('click',()=>{
+document.querySelector('.btn-hold').addEventListener('click',function(){
     scores[activePlayer]+=roundScore;
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
     if(scores[activePlayer] >= 10){
@@ -56,6 +41,8 @@ document.querySelector('.btn-hold').addEventListener('click',()=>{
         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
         document.querySelector('.player-0-panel').classList.toggle('active');
         document.querySelector('.player-1-panel').classList.toggle('active');
+
+
 
  }
     else{
@@ -75,6 +62,40 @@ function nextPlayer(){
 
     document.querySelector('.dice').style.display = 'none';
 };
+
+document.querySelector('.btn-new').addEventListener('click',init);
+
+function init(){
+    scores = [0,0]
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.querySelector('.dice').style.display = 'none';
+
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.getElementById('name-0').textContent = "Player 0";
+    document.getElementById('name-1').textContent = "Player 1";
+    document.querySelector('.player-' + 0 + '-panel').classList.remove('winner');
+    document.querySelector('.player-' + 1 + '-panel').classList.remove('winner');
+    document.querySelector('.player-' + 0 + '-panel').classList.remove('active');
+    document.querySelector('.player-' + 1 + '-panel').classList.remove('active');
+    document.querySelector('.player-' + 0 + '-panel').classList.add('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
